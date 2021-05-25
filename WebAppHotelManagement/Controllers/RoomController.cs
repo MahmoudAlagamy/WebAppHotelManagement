628853ffc5f69aae5aef0e823bdeee99939748bf
MahmoudAlagamy <81970125+MahmoudAlagamy@users.noreply.github.com>
2021-05-25 23:37:00 +0200

    Add project files.

---
+++ WebAppHotelManagement/Controllers/RoomController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppHotelManagement.Models;
+using WebAppHotelManagement.ViewModel;
+
+namespace WebAppHotelManagement.Controllers
+{
+    public class RoomController : Controller
+    {
+        readonly HotelDBEntities objHotelDBEntities;
+        public RoomController()
+        {
+            objHotelDBEntities = new HotelDBEntities();
+        }
+        // GET: Room
+        public ActionResult Index()
+        {
+            RoomViewModel objRoomViewModel = new RoomViewModel
+            {
+                ListOfBookingStatus = (from obj in objHotelDBEntities.BookingStatus
+                                       select new SelectListItem()
+                                       {
+                                           Text = obj.BookingStatus,
+                                           Value = obj.BookingStatusId.ToString(),
+
+                                       }).ToList(),
+
+                ListOfRoomType = (from obj in objHotelDBEntities.RoomTypes
+                                  select new SelectListItem()
+                                  {
+                                      Text = obj.RoomTypeName,
+                                      Value = obj.RoomTypeId.ToString(),
+
+                                  }).ToList()
+            };
+
+            return View(objRoomViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(RoomViewModel objRoomViewModel)
+        {
+            string ImageUniqueName = Guid.NewGuid().ToString();
+            string ActualImageName = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+            objRoomViewModel.Image.SaveAs(Server.MapPath("~/RoomImages/" + ActualImageName));
+
+            Room objRoom = new Room()
+            {
+                RoomNumber = objRoomViewModel.RoomNumber,
+                RoomDescription = objRoomViewModel.RoomDescription,
+                RoomPrice = objRoomViewModel.RoomPrice,
+                BookingStatusId = objRoomViewModel.BookingStatusId,
+                IsActive = true,
+                RoomImage = ActualImageName,
+                RoomCapacity = objRoomViewModel.RoomCapacity,
+                RoomTypeId = objRoomViewModel.RoomTypeId
+
+            };
+
+            objHotelDBEntities.Rooms.Add(objRoom);
+            objHotelDBEntities.SaveChanges();
+
+            return Json(new { message = "Room Successfully Added.", success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public PartialViewResult GetAllRooms()
+        {
+            IEnumerable<RoomDetailsViewModel> listOfRoomDetailsViewModel =
+                (from objRoom in objHotelDBEntities.Rooms
+                 join objBooking in objHotelDBEntities.BookingStatus on objRoom.BookingStatusId equals objBooking.BookingStatusId
+                 join objRoomType in objHotelDBEntities.RoomTypes on objRoom.RoomTypeId equals objRoomType.RoomTypeId
+                 select new RoomDetailsViewModel()
+                 {
+                     RoomNumber = objRoom.RoomNumber,
+                     RoomDescription = objRoom.RoomDescription,
+                     RoomCapacity = objRoom.RoomCapacity,
+                     RoomPrice = objRoom.RoomPrice,
+                     BookingStatus = objBooking.BookingStatus,
+                     RoomType = objRoomType.RoomTypeName,
+                     RoomImage = objRoom.RoomImage,
+                     RoomId = objRoom.RoomId
+
+                 }).ToList();
+            return PartialView("_RoomDetailsPartial", listOfRoomDetailsViewModel);
+        }
+
+        [HttpGet]
+        public JsonResult EditRoomDetails(int roomId)
+        {
+            var result = objHotelDBEntities.Rooms.Single(model => model.RoomId == roomId);
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WebAppHotelManagement/Scripts/ScriptFile.js
@@ -0,0 +1,112 @@
+﻿
+
+$(document).ready(function () {
+    LoadRoomDetails();
+
+    $("#btnSave").click(function () {
+        SaveRoomData();
+    });
+
+
+});
+
+function LoadRoomDetails() {
+    $.ajax({
+        async: true,
+        data: 'GET',
+        dataType: 'HTML',
+        contentType: false,
+        processData: false,
+        url: '/Room/GetAllRooms',
+        success: function (data) {
+            $("#divLoadRoomDetails").html(data);
+        },
+        error: function() {
+            alert("There is some Problem to proccess your Request.");
+        }
+    });
+}
+
+function DeleteRoom(roomId) {
+    alert(roomId);
+}
+
+function EditRoom(roomId) {
+    
+    $.ajax({
+        async: true,
+        type: 'GET',
+        dataType: 'JSON',
+        contentType: 'application/json; charset=utf-8',
+        data: { roomId: roomId },
+        url: '/Room/EditRoomDetails',
+        success: function (data) {
+            $("#txtRoomNumber").val(data.RoomNumber);
+            $("#txtRoomPrice").val(data.RoomPrice);
+            $("#ddBookingStatus").val(data.BookingStatusId);
+            $("#ddRoomType").val(data.RoomTypeId);
+            $("#txtRoomCapacity").val(data.RoomCapacity);
+            $("#txtRoomDescription").val(data.RoomDescription);
+            $("#imageRoom").attr('src', "../RoomImages/" + data.RoomImage);
+            $("#divAddRoom").modal({ show: true });
+
+        },
+        error: function () {
+            alert("There is some Problem to proccess your Request.");
+        }
+    });
+}
+
+function SaveRoomData() {
+    var formData = new FormData;
+    formData.append("RoomNumber", $("#txtRoomNumber").val());
+    formData.append("RoomPrice", $("#txtRoomPrice").val());
+    formData.append("BookingStatusId", $("#ddBookingStatus").val());
+    formData.append("RoomTypeId", $("#ddRoomType").val());
+    formData.append("RoomCapacity", $("#txtRoomCapacity").val());
+    formData.append("RoomDescription", $("#txtRoomDescription").val());
+    formData.append("Image", $("#uploadImage").get(0).files[0]);
+
+    $.ajax({
+        async: true,
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function (data) {
+            if (data.success === true) {
+                alert(data.message);
+                ResetRoomInformation();
+                LoadRoomDetails();
+            }
+        },
+        error: function () {
+            alert("There Is Some Problem To Proccess Your Request Please Try Again Later.");
+        }
+    });
+}
+
+function ResetRoomInformation() {
+    $("#txtRoomNumber").val('');
+    $("#txtRoomPrice").val('');
+    $("#ddBookingStatus").val(1);
+    $("#ddRoomType").val(1);
+    $("#txtRoomCapacity").val('');
+    $("#txtRoomDescription").val('');
+    $("#uploadImage").val('');
+    $("#txtRoomNumber").focus();
+    $("#imageRoom").removeAttr('src');
+
+}
+
+function DisplayImage(result) {
+
+    if (result.files && result.files[0]) {
+        var fileReader = new FileReader();
+        fileReader.onload = function (e) {
+            $("#imageRoom").attr('src', e.target.result);
+        }
+        fileReader.readAsDataURL(result.files[0]);
+    }
+
+}
